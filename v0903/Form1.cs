6d611987aceb0d87976a6118199172cde87411e5
A601-12 <mkii.dat@gmail.com>
2020-09-03 12:09:16 +0900

    とりあえずコミット

--- v0903/Form1.cs
+++ v0903/Form1.cs
@@ -14,24 +14,33 @@ namespace v0903
     {
         private static Random rand = new Random();
 
-        int vx = rand.Next(-20, 21);
-        int vy = rand.Next(-20, 21);
-        int vx2 = rand.Next(-20, 21);
-        int vy2 = rand.Next(-20, 21);
-        int vx3 = rand.Next(-20, 21);
-        int vy3 = rand.Next(-20, 21);
+        int [] vx = new int[3];
+        int [] vy = new int[3];
+        Label[] labels = new Label[100];
 
         public Form1()
         {
             InitializeComponent();
 
+            for(int i=0;i < 3;i++)
+            {
+                vx[i] = rand.Next(-20,21);
+                vy[i] = rand.Next(-20,21);
+            }
+            labels[0] = new Label();
+            labels[0].AutoSize = true;
+            labels[0].Text = "＼(・ω・)／";
+            Controls.Add(labels[0]);
+
+            labels[0].Left = rand.Next(ClientSize.Width - label1.Width);
+            labels[0].Top = rand.Next(ClientSize.Height - label1.Height);
 
             label1.Left = rand.Next(ClientSize.Width - label1.Width);
             label1.Top = rand.Next(ClientSize.Height - label1.Height);
-            label2.Left = rand.Next(ClientSize.Width - label1.Width);
-            label2.Top = rand.Next(ClientSize.Height - label1.Height);
-            label3.Left = rand.Next(ClientSize.Width - label1.Width);
-            label3.Top = rand.Next(ClientSize.Height - label1.Height);
+            label2.Left = rand.Next(ClientSize.Width - label2.Width);
+            label2.Top = rand.Next(ClientSize.Height - label2.Height);
+            label3.Left = rand.Next(ClientSize.Width - label3.Width);
+            label3.Top = rand.Next(ClientSize.Height - label3.Height);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -43,84 +52,85 @@ namespace v0903
         {
             bool pien = false;
 
-            label1.Left += vx;
-            label1.Top += vy;
+            label1.Left += vx[0];
+            label1.Top += vy[0];
 
             if(label1.Left <= 0)
             {
-                vx = Math.Abs(vx);
+                vx[0] = Math.Abs(vx[0]);
                 label1.Text = "(／・ω・)／";
             }
             if(label1.Top <= 0)
             {
-                vy = Math.Abs(vy);
+                vy[0] = Math.Abs(vy[0]);
                 label1.Text = "／(・ω・)＼";
             }
             if(label1.Right >= ClientSize.Width)
             {
-                vx = -Math.Abs(vx);
+                vx[0] = -Math.Abs(vx[0]);
                 label1.Text = "＼(・ω・＼)";
             }
             if(label1.Bottom >= ClientSize.Height)
             {
-                vy = -Math.Abs(vy);
+                vy[0] = -Math.Abs(vy[0]);
                 label1.Text = "＼(・ω・)／";
             }
 
-            label2.Left += vx2;
-            label2.Top += vy2;
 
-            if (label2.Left <= 0)
+            label2.Left += vx[1];
+            label2.Top += vy[1];
+
+            if (label2.Left <= 1)
             {
-                vx2 = Math.Abs(vx2);
+                vx[1] = Math.Abs(vx[1]);
                 label2.Text = "(／・ω・)／";
             }
             if (label2.Top <= 0)
             {
-                vy2 = Math.Abs(vy2);
+                vy[1] = Math.Abs(vy[1]);
                 label2.Text = "／(・ω・)＼";
             }
             if (label2.Right >= ClientSize.Width)
             {
-                vx2 = -Math.Abs(vx2);
+                vx[1] = -Math.Abs(vx[1]);
                 label2.Text = "＼(・ω・＼)";
             }
             if (label2.Bottom >= ClientSize.Height)
             {
-                vy2 = -Math.Abs(vy2);
+                vy[1] = -Math.Abs(vy[1]);
                 label2.Text = "＼(・ω・)／";
             }
 
-            label3.Left += vx3;
-            label3.Top += vy3;
+            label3.Left += vx[2];
+            label3.Top += vy[2];
 
             if (label3.Left <= 0)
             {
-                vx3 = Math.Abs(vx3);
+                vx[2] = Math.Abs(vx[2]);
                 label3.Text = "(／・ω・)／";
             }
             if (label3.Top <= 0)
             {
-                vy3 = Math.Abs(vy3);
+                vy[2] = Math.Abs(vy[2]);
                 label3.Text = "／(・ω・)＼";
             }
             if (label3.Right >= ClientSize.Width)
             {
-                vx3 = -Math.Abs(vx3);
+                vx[2] = -Math.Abs(vx[2]);
                 label3.Text = "＼(・ω・＼)";
             }
             if (label3.Bottom >= ClientSize.Height)
             {
-                vy3 = -Math.Abs(vy3);
+                vy[2] = -Math.Abs(vy[2]);
                 label3.Text = "＼(・ω・)／";
             }
 
             Point mp = MousePosition;
             mp = PointToClient(mp);
-            if(     mp.X > label1.Left 
-                &&  mp.X < label1.Right
-                &&  mp.Y > label1.Top
-                &&  mp.Y < label1.Bottom)
+            if (mp.X > label1.Left
+                && mp.X < label1.Right
+                && mp.Y > label1.Top
+                && mp.Y < label1.Bottom)
             {
                 if (!pien)
                 {
@@ -129,13 +139,14 @@ namespace v0903
                     pien = true;
                     MessageBox.Show("ぴえん");
                     timer1.Enabled = true;
-                    vx = rand.Next(-20, 21);
-                    vy = rand.Next(-20, 21);
+                    vx[0] = rand.Next(-20, 21);
+                    vy[0] = rand.Next(-20, 21);
                     label1.Left = rand.Next(ClientSize.Width - label1.Width);
                     label1.Top = rand.Next(ClientSize.Height - label1.Height);
                     label1.Text = "＼(・ω・)／";
                 }
             }
+
         }
     }
 }
