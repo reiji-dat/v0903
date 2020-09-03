f5d18065acf86ae2dcb641e62f089dceb835df1f
A601-12 <mkii.dat@gmail.com>
2020-09-03 10:55:51 +0900

    とりあえず3つ

--- v0903/Form1.cs
+++ v0903/Form1.cs
@@ -13,13 +13,25 @@ namespace v0903
     public partial class Form1 : Form
     {
         private static Random rand = new Random();
+
         int vx = rand.Next(-20, 21);
         int vy = rand.Next(-20, 21);
+        int vx2 = rand.Next(-20, 21);
+        int vy2 = rand.Next(-20, 21);
+        int vx3 = rand.Next(-20, 21);
+        int vy3 = rand.Next(-20, 21);
+
         public Form1()
         {
             InitializeComponent();
+
+
             label1.Left = rand.Next(ClientSize.Width - label1.Width);
             label1.Top = rand.Next(ClientSize.Height - label1.Height);
+            label2.Left = rand.Next(ClientSize.Width - label1.Width);
+            label2.Top = rand.Next(ClientSize.Height - label1.Height);
+            label3.Left = rand.Next(ClientSize.Width - label1.Width);
+            label3.Top = rand.Next(ClientSize.Height - label1.Height);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -29,10 +41,10 @@ namespace v0903
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            bool pien = false;
+
             label1.Left += vx;
             label1.Top += vy;
-
-            bool pien = false;
 
             if(label1.Left <= 0)
             {
@@ -55,6 +67,54 @@ namespace v0903
                 label1.Text = "＼(・ω・)／";
             }
 
+            label2.Left += vx2;
+            label2.Top += vy2;
+
+            if (label2.Left <= 0)
+            {
+                vx2 = Math.Abs(vx2);
+                label2.Text = "(／・ω・)／";
+            }
+            if (label2.Top <= 0)
+            {
+                vy2 = Math.Abs(vy2);
+                label2.Text = "／(・ω・)＼";
+            }
+            if (label2.Right >= ClientSize.Width)
+            {
+                vx2 = -Math.Abs(vx2);
+                label2.Text = "＼(・ω・＼)";
+            }
+            if (label2.Bottom >= ClientSize.Height)
+            {
+                vy2 = -Math.Abs(vy2);
+                label2.Text = "＼(・ω・)／";
+            }
+
+            label3.Left += vx3;
+            label3.Top += vy3;
+
+            if (label3.Left <= 0)
+            {
+                vx3 = Math.Abs(vx3);
+                label3.Text = "(／・ω・)／";
+            }
+            if (label3.Top <= 0)
+            {
+                vy3 = Math.Abs(vy3);
+                label3.Text = "／(・ω・)＼";
+            }
+            if (label3.Right >= ClientSize.Width)
+            {
+                vx3 = -Math.Abs(vx3);
+                label3.Text = "＼(・ω・＼)";
+            }
+            if (label3.Bottom >= ClientSize.Height)
+            {
+                vy3 = -Math.Abs(vy3);
+                label3.Text = "＼(・ω・)／";
+            }
+
             Point mp = MousePosition;
             mp = PointToClient(mp);
             if(     mp.X > label1.Left 
@@ -74,7 +134,6 @@ namespace v0903
                     label1.Left = rand.Next(ClientSize.Width - label1.Width);
                     label1.Top = rand.Next(ClientSize.Height - label1.Height);
                     label1.Text = "＼(・ω・)／";
-
                 }
             }
         }
